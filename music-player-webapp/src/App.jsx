ed693806e1a3031b30db810e3812f58d18dcc00b
karol-prog42 <karljatienza@gmail.com>
2026-04-01 15:20:16 +0800

    Debugging

--- music-player-webapp/src/App.jsx
+++ music-player-webapp/src/App.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/NavBar";
 import Logo from "./components/Logo";
 import Result from "./components/Result";
